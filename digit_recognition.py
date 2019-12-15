b0030ac9567a86a291fa71d89c662ba01e6047b5
Glushkov Vladimir <glushkov.vk@students.dvfu.ru>
2019-12-15 14:44:30 +1000

    Add model saving

--- digit_recognition.py
+++ digit_recognition.py
@@ -8,18 +8,24 @@ from utils import check_file_exists, get_cwd
 
 
 class ProgressHistory(keras.callbacks.Callback):
-    on_train_begin_callback: Callable[[], int] = None
+    on_epoch_begin_callback: Callable[[], int] = None
     on_batch_begin_callback: Callable[[], int] = None
-
-    def on_train_begin(self, logs=None):
-        if self.on_train_begin_callback is None:
-            return
-        self.on_train_begin_callback()
+    on_train_end_callback: Callable[[], int] = None
 
     def on_batch_end(self, batch, logs=None):
         if self.on_batch_begin_callback is None:
             return
         self.on_batch_begin_callback()
+
+    def on_train_end(self, logs=None):
+        if self.on_train_end_callback is None:
+            return
+        self.on_train_end_callback()
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if self.on_epoch_begin_callback is None:
+            return
+        self.on_epoch_begin_callback()
 
 class MyModel:
     def __init__(self):
@@ -30,7 +36,7 @@ class MyModel:
         self.epochs = 12
 
         img_rows = img_cols = 28
-        (x_train, y_train), (x_test, y_test) = mnist.load_data(get_cwd() + "/.keras/datasets/data/mnist.npz")
+        (x_train, y_train), (x_test, y_test) = mnist.load_data(get_cwd() + "/.keras/datasets/mnist.npz")
 
         self.x_train = x_train.reshape(60000, 28, 28, 1)
         self.x_test = x_test.reshape(10000, 28, 28, 1)
@@ -44,27 +50,32 @@ class MyModel:
 
         self.model = None
 
-    def train(self, ):
-        with self:
-            model = Sequential()
-            model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
-            model.add(Conv2D(64, (3, 3), activation='relu'))
-            model.add(MaxPooling2D(pool_size=(2, 2)))
-            model.add(Dropout(0.25))
-            model.add(Flatten)
-            model.add(Dense(128, activation='relu'))
-            model.add(Dropout(0.5))
-            model.add(Dense(self.num_classes, activation='softmax'))
-            model.compile(
-                loss=keras.losses.categorical_crossentropy,
-                optimizer=keras.optimizers.Adadelta(),
-                metrics=['accuracy']
-            )
-            model.fit(
-                self.x_train,
-                self.y_train,
-                batch_size=self.batch_size,
-                epochs=self.epochs,
-                verbose=1,
-                validation_data=(self.x_test, self.y_test)
-            )
+    def train(self, pb_history: ProgressHistory):
+        self.model = Sequential()
+        self.model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.25))
+        self.model.add(Flatten())
+        self.model.add(Dense(128, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(self.num_classes, activation='softmax'))
+        self.model.compile(
+            loss=keras.losses.categorical_crossentropy,
+            optimizer=keras.optimizers.Adadelta(),
+            metrics=['accuracy']
+        )
+        self.model.fit(
+            self.x_train,
+            self.y_train,
+            batch_size=self.batch_size,
+            epochs=self.epochs,
+            verbose=1,
+            validation_data=(self.x_test, self.y_test),
+            callbacks=[pb_history]
+        )
+
+        with open(get_cwd() + "/.keras/models/model.yaml", "w+") as yaml_file:
+            yaml_file.write(self.model.to_yaml())
+        self.model.save_weights(get_cwd() + "/.keras/models/model.h5")
+        print("Model was saved to disk.")
